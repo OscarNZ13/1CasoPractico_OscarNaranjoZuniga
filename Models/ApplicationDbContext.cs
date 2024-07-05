272595031dfebe3552b2e13a7a00effd30a7e8db
OscarNZ13 <onaranjo00330@ufide.ac.cr>
2024-07-04 20:28:09 -0600

    Todo completo menos lo de la imagen

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -1,23 +1,49 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using _1CasoPractico_OscarNaranjoZuniga.Models;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore;
 
-namespace _1CasoPractico_OscarNaranjoZuniga.Models
+public class ApplicationDbContext : DbContext
 {
-    public class ApplicationDbContext : DbContext
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+    }
+
+    public DbSet<Servicio> Servicios { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Servicio>()
+            .Property(s => s.Costo)
+            .HasColumnType("decimal(18,2)");
+
+        modelBuilder.Entity<Servicio>()
+            .Property(s => s.FechaCreacion)
+            .HasDefaultValueSql("GETDATE()");
+
+        modelBuilder.Entity<Servicio>()
+            .Property(s => s.FechaModificacion)
+            .HasDefaultValueSql("GETDATE()");
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public override int SaveChanges()
+    {
+        foreach (var entry in ChangeTracker.Entries<Servicio>())
         {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Entity.FechaCreacion = DateTime.Now;
+            }
+            else if (entry.State == EntityState.Modified)
+            {
+                entry.Entity.FechaModificacion = DateTime.Now;
+                // No cambiar la FechaCreacion en la edición
+                entry.Property(s => s.FechaCreacion).IsModified = false;
+            }
         }
 
-        public DbSet<Servicio> Servicios { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Servicio>()
-                .Property(s => s.Costo)
-                .HasColumnType("decimal(18,2)");
-
-            base.OnModelCreating(modelBuilder);
-        }
+        return base.SaveChanges();
     }
 }
